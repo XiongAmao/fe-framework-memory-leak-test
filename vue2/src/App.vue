78d3ac521fdac344460d375f707e381b719b45e1
XiongAmao <onycat@live.cn>
2021-04-23 15:36:10 +0800

    add vue2 test

---
+++ vue2/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="App">
+    <div>
+      <br>
+      <button @click="fff">切换</button>
+    </div>
+    <br>
+    <br>
+    <test v-if="status" :key="key"/>
+    
+  </div>
+</template>
+
+<script>
+  import Test from './TestA.vue'
+
+  export default {
+    name: 'App',
+    components: { Test },
+    props: {},
+    data() {
+      return {
+        status: true,
+        key: 123
+      }
+    },
+    created() {},
+    methods: {
+      fff() {
+        this.key++
+        this.status = !this.status
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .App {
+  }
+</style>
---
+++ vue2/src/TestA.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <button
+      class="button"
+      @click="start"
+    >开始</button>
+
+    <div>text: {{ wtf }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    props: {},
+    data() {
+      return {
+        wtf: 11,
+        list: [],
+        timer: null,
+      }
+    },
+    created() {
+      const list = []
+      for (let i = 0; i < 10000; i++) {
+        list.push({
+          label: 'Item ' + i,
+          value: i,
+        })
+      }
+      this.list = list
+    },
+    destroyed() {
+      // 对比使用这个跟不使用的区别
+      // clearTimeout(this.timer)
+    },
+    methods: {
+      start() {
+        console.log('start')
+        this.timer = setTimeout(() => {
+          console.log(this)
+          this.wtf += 99
+        }, 2000)
+      },
+    },
+  }
+</script>
